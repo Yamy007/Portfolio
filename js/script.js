904d80a8ad954cff43e31d77c6323ee17780b429
Yamy007 <yamy19007@gmail.com>
2023-04-26 13:00:14 +0200

    innit commit
    *add project in git

---
+++ js/script.js
@@ -0,0 +1,58 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('act');
+    
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+   let header = document.querySelector('header');
+
+   header.classList.toggle('sticky', window.scrollY > 100);
+
+   menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+   
+};
+
+ScrollReveal({reset:false,
+distance: '80px',
+duration: 2000,
+delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, .heading', {origin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin:'bottom'});
+ScrollReveal().reveal('.about-img, .home-content h1', {origin:'left'});
+ScrollReveal().reveal('.about-content, .home-content p', {origin:'bottom'});
+
+
+
+const typed = new Typed('.multiple-text', {
+    strings: ["Fullstack Developer", "Python Django", "React JS"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
